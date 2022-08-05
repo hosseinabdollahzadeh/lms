88544e09a391fbe67cd15425a5910f36c19e5aff
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-05 20:26:40 +0430

    login by multiple columns

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function credentials(Request $request)
+    {
+        $username = $request->get($this->username());
+
+        $field = filter_var($username, FILTER_VALIDATE_EMAIL) ? 'email' : 'mobile';
+
+        return [
+            $field => $username,
+            'password' => $request->password
+        ];
     }
 }

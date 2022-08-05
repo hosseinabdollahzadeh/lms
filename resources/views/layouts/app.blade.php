47ea14d3e683ea9f7439227168afa8483960f92e
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-05 22:31:23 +0430

    fix error in home page (that caused with not install npm)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+{{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
 </head>
 <body>
     <div id="app">

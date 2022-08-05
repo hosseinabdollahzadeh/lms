87dc6b46ee317f9854fd9c83b2ff444e04caf296
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-05 22:08:11 +0430

    reset password

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,33 @@
+@extends('auth.master')
+
+@section('content')
+
+    <form method="POST" action="{{ route('password.email') }}" class="form">
+        @csrf
+        <a class="account-logo" href="/">
+            <img src="/img/weblogo.png" alt="">
+        </a>
+        <div class="form-content form-account">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <input id="email" type="email" class="txt-l txt @error('email') is-invalid @enderror" placeholder="ایمیل"
+                   name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+            <br>
+            <button type="submit" class="btn btn-recoverpass">بازیابی</button>
+        </div>
+        <div class="form-footer">
+            <a href="login.html">صفحه ورود</a>
+        </div>
+    </form>
+
+@endsection

9589ad04cfadf52ee035d3c5a76b19742b3caed9
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-08 13:43:57 +0430

    Transfer Migrations & Factories of User module

--- modules/Abd/User/Providers/UserServiceProvider.php
+++ modules/Abd/User/Providers/UserServiceProvider.php
@@ -9,5 +9,7 @@ class UserServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../Routes/user_routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+        $this->loadFactoriesFrom(__DIR__.'/../Database/Factories');
     }
 }

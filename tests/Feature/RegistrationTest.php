6e585300007f6d231afa43bb6a2718eace18a6eb
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-10 21:05:16 +0430

    test register form

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use Abd\User\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_user_can_see_register_form()
+    {
+        $response = $this->get(route('register'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_register()
+    {
+        $this->withExceptionHandling();
+
+        $response = $this->registerNewUser();
+
+        $response->assertRedirect(route('home'));
+
+        $this->assertCount(1, User::all());
+    }
+
+    public function test_user_have_to_verify_email()
+    {
+        $this->registerNewUser();
+        $response = $this->get(route('home'));
+
+        $response->assertRedirect(route('verification.notice'));
+    }
+
+    public function test_verified_user_can_see_home_page()
+    {
+        $this->registerNewUser();
+
+        $this->assertAuthenticated();
+
+        auth()->user()->markEmailAsVerified();
+
+        $response = $this->get(route('home'));
+
+        $response->assertOk();
+    }
+
+    private function registerNewUser()
+    {
+        return $this->post(route('register'), [
+            'name' => 'hossein',
+            'email' => 'hossein@test.test',
+            'mobile' => '9123456789',
+            'password' => 'aA!123',
+            'password_confirmation' => 'aA!123'
+        ]);
+    }
+}

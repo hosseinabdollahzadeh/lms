429268d4182cff511ca77c128e02913717644fe4
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-15 13:45:12 +0430

    verification test

--- modules/Abd/User/Tests/Feature/RegistrationTest.php
+++ modules/Abd/User/Tests/Feature/RegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Abd\User\Tests\Feature;
 
 use Abd\User\Models\User;
+use Abd\User\Services\VerifyCodeService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -34,12 +35,31 @@ class RegistrationTest extends TestCase
         $this->assertCount(1, User::all());
     }
 
-    public function test_user_have_to_verify_email()
+    public function test_user_have_to_verify_account()
     {
         $this->registerNewUser();
         $response = $this->get(route('home'));
 
         $response->assertRedirect(route('verification.notice'));
+    }
+
+    public function test_user_can_verify_account()
+    {
+        $user = User::create([
+            'name' => 'hossein',
+            'email' => 'test@test.test',
+            'password' => bcrypt('aA!123')
+        ]);
+
+        $code = VerifyCodeService::generate();
+        VerifyCodeService::store($user->id, $code);
+        auth()->loginUsingId($user->id);
+        $this->assertAuthenticated();
+
+        $this->post(route('verification.verify'), [
+            'verify_code' => $code
+        ]);
+        $this->assertEquals(true, $user->fresh()->hasVerifiedEmail());
     }
 
     public function test_verified_user_can_see_home_page()

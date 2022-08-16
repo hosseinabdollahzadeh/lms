c32d7d14e8f4ad3dda8ceda45dc52aa54d00f17c
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-16 16:16:11 +0430

    config dashboard middlewares

--- modules/Abd/Dashboard/Routes/dashboard_routes.php
+++ modules/Abd/Dashboard/Routes/dashboard_routes.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['namespace'=>'Abd\Dashboard\Http\Controllers'], function ($router){
+Route::group(['namespace'=>'Abd\Dashboard\Http\Controllers', 'middleware'=> ['web', 'auth', 'verified']], function ($router){
     $router->get('/home','DashboardController@home')->name('home');
 });

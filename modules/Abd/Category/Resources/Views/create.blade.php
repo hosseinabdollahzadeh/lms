45750a3cd1b8d568a0b6dd5147a0c382c6830780
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-24 21:28:38 +0430

    Add error message to create category

--- modules/Abd/Category/Resources/Views/create.blade.php
+++ modules/Abd/Category/Resources/Views/create.blade.php
@@ -2,7 +2,19 @@
 <form action="{{ route('categories.store') }}" method="post" class="padding-30">
     @csrf
     <input type="text" name="title" placeholder="نام دسته بندی" class="text">
+    @error('title')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+    @enderror
+
     <input type="text" name="slug" placeholder="نام انگلیسی دسته بندی" class="text">
+    @error('slug')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+    @enderror
+
     <p class="box__title margin-bottom-15">انتخاب دسته پدر</p>
     <select name="parent_id" id="parent_id">
         <option value="">ندارد</option>
@@ -10,5 +22,11 @@
             <option value="{{$category->id}}">{{$category->title}}</option>
         @endforeach
     </select>
+    @error('parent_id')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+    @enderror
+    <hr>
     <button class="btn btn-brand">اضافه کردن</button>
 </form>

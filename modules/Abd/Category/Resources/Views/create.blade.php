ccd75dbf789b512256ad55d67ade2b8da78e621d
hosseinabdollahzadeh <abdollahzadeh.hossein@gmail.com>
2022-08-24 21:32:00 +0430

    Add old value to create category form

--- modules/Abd/Category/Resources/Views/create.blade.php
+++ modules/Abd/Category/Resources/Views/create.blade.php
@@ -1,14 +1,14 @@
 <p class="box__title">ایجاد دسته بندی جدید</p>
 <form action="{{ route('categories.store') }}" method="post" class="padding-30">
     @csrf
-    <input type="text" name="title" placeholder="نام دسته بندی" class="text">
+    <input type="text" name="title" placeholder="نام دسته بندی" class="text" value="{{old("title")}}">
     @error('title')
     <span class="invalid-feedback" role="alert">
         <strong>{{ $message }}</strong>
     </span>
     @enderror
 
-    <input type="text" name="slug" placeholder="نام انگلیسی دسته بندی" class="text">
+    <input type="text" name="slug" placeholder="نام انگلیسی دسته بندی" class="text" value="{{old("slug")}}">
     @error('slug')
     <span class="invalid-feedback" role="alert">
         <strong>{{ $message }}</strong>
@@ -19,7 +19,7 @@
     <select name="parent_id" id="parent_id">
         <option value="">ندارد</option>
         @foreach($categories as $category)
-            <option value="{{$category->id}}">{{$category->title}}</option>
+            <option value="{{old($category->id)}}">{{$category->title}}</option>
         @endforeach
     </select>
     @error('parent_id')

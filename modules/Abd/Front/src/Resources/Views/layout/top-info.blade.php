2cb7120ad814a3dd8910351774d879e13148e360
unknown <abdollahzadeh.hossein@gmail.com>
2022-10-17 03:35:50 +0330

    Organize blade files

---
+++ modules/Abd/Front/src/Resources/Views/layout/top-info.blade.php
@@ -0,0 +1,7 @@
+<div class="top-info">
+    @include('Front::layout.slider')
+    <div class="optionals">
+        <div><img src="img/banner/laravel-payment-processing.jpg" alt=""></div>
+        <div><img src="img/banner/laravel-payment-processing.jpg" alt=""></div>
+    </div>
+</div>

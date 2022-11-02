a2740370fb027c86fa406a5bff23b68e2b95ab80
unknown <abdollahzadeh.hossein@gmail.com>
2022-11-02 15:11:18 +0330

    Control latst pending settlement

--- modules/Abd/Payment/src/Http/Controllers/SettlementController.php
+++ modules/Abd/Payment/src/Http/Controllers/SettlementController.php
@@ -15,25 +15,44 @@ class SettlementController extends Controller
         return view('Payment::settlements.index', compact('settlements'));
     }
 
-    public function create()
+    public function create(SettlementRepo $repo)
     {
+        if ($repo->getLatestPendingSettlement(auth()->id())) {
+            newFeedback("ناموفق", "شما یک درخواست در حال انتظار دارید و فعلا نمی توانید درخواست جدیدی ثبت نمایید.", "error");
+            return redirect(route('settlements.index'));
+        }
         return view('Payment::settlements.create');
     }
 
-    public function store(SettlementRequest $request)
+    public function store(SettlementRequest $request, SettlementRepo $repo)
     {
+        if ($repo->getLatestPendingSettlement(auth()->id())) {
+            newFeedback("ناموفق", "شما یک درخواست در حال انتظار دارید و فعلا نمی توانید درخواست جدیدی ثبت نمایید.", "error");
+            return redirect(route('settlements.index'));
+        }
         SettlementService::store($request->all());
         return redirect(route('settlements.index'));
     }
 
     public function edit($settlementId, SettlementRepo $repo)
     {
-        $settlement = $repo->find($settlementId);
+        $requestedSettlement = $repo->find($settlementId);
+        $settlement = $repo->getLatestSettlement($requestedSettlement->user_id);
+        if ($settlement->id != $settlementId) {
+            newFeedback("ناموفق", "این درخواست تسویه قابل ویرایش نیست و بایگانی شده است. فقط آخرین درخواست تسویه ی هر کاربر قابل ویرایش است.", "error");
+            return redirect(route('settlements.index'));
+        }
         return view('Payment::settlements.edit', compact('settlement'));
     }
 
-    public function update($settlementId, SettlementRequest $request)
+    public function update($settlementId, SettlementRequest $request, SettlementRepo $repo)
     {
+        $requestedSettlement = $repo->find($settlementId);
+        $settlement = $repo->getLatestSettlement($requestedSettlement->user_id);
+        if ($settlement->id != $settlementId) {
+            newFeedback("ناموفق", "این درخواست تسویه قابل ویرایش نیست و بایگانی شده است. فقط آخرین درخواست تسویه ی هر کاربر قابل ویرایش است.", "error");
+            return redirect(route('settlements.index'));
+        }
         SettlementService::update($settlementId, $request->all());
         return redirect(route('settlements.index'));
     }

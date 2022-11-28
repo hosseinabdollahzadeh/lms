1ce97c9c1d628a9507dd6caf61260ae285625c6b
hossein <abdollahzadeh.hossein@gmail.com>
2022-11-28 20:37:09 +0330

    ticket request validation

--- modules/Abd/Ticket/src/Http/Controllers/TicketController.php
+++ modules/Abd/Ticket/src/Http/Controllers/TicketController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Abd\Ticket\Http\Controllers;
 
+use Abd\Ticket\Http\Requests\TicketRequest;
 use Abd\Ticket\Repositories\TicketRepo;
 use App\Http\Controllers\Controller;
 
@@ -16,5 +17,10 @@ class TicketController extends Controller
     public function create()
     {
         return view("Tickets::create");
+    }
+
+    public function store(TicketRequest $request)
+    {
+
     }
 }
--- modules/Abd/Ticket/src/Resources/Views/create.blade.php
+++ modules/Abd/Ticket/src/Resources/Views/create.blade.php
@@ -7,10 +7,10 @@
         <p class="box__title">ایجاد تیکت جدید</p>
         <div class="row no-gutters bg-white">
             <div class="col-12">
-                <form action="" class="padding-30">
+                <form action="{{route("tickets.store")}}" method="post" enctype="multipart/form-data" class="padding-30">
+                    @csrf
                     <x-input type="text" class="text" name="title" placeholder="عنوان تیکت" required />
                     <x-textarea placeholder="متن تیکت" name="body" class="text" required/>
-
                     <x-file name="attachment" placeholder="آپلود فایل پیوست"/>
                     <button class="btn btn-brand">ایجاد مقاله</button>
                 </form>

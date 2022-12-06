4446c96bb7e6ea68af2dc59c02d3cb4b2f956902
hossein <abdollahzadeh.hossein@gmail.com>
2022-12-06 17:19:36 +0330

    send telegram notifications

--- modules/Abd/Comment/src/Notifications/CommentSubmittedNotification.php
+++ modules/Abd/Comment/src/Notifications/CommentSubmittedNotification.php
@@ -4,8 +4,8 @@ namespace Abd\Comment\Notifications;
 
 use Abd\Comment\Mail\CommentSubmittedMail;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramMessage;
 use function url;
 
 class CommentSubmittedNotification extends Notification
@@ -19,12 +19,30 @@ class CommentSubmittedNotification extends Notification
 
     public function via($notifiable) : array
     {
-        return ['mail'];
+        return ['mail', 'telegram'];
     }
 
     public function toMail($notifiable)
     {
         return (new CommentSubmittedMail($this->comment))->to($notifiable->email);
+    }
+
+    public function toTelegram($notifiable)
+    {
+        return TelegramMessage::create()
+            // Optional recipient user id.
+            ->to(967259502)
+            // Markdown supported.
+            ->content("یک دیدگاه جدید برای دوره ی شما در وب آموز ارسال شده است.")
+
+            // (Optional) Blade template for the content.
+            // ->view('notification', ['url' => $url])
+
+            // (Optional) Inline Buttons
+            ->button('مشاهده ی دوره', $this->comment->commentable->path())
+            ->button('مدیریت دیدگاه ها', route('comments.index'));
+            // (Optional) Inline Button with callback. You can handle callback in your bot instance
+//            ->buttonWithCallback('Confirm', 'confirm_invoice ' . $this->invoice->id);
     }
 
     public function toArray($notifiable) : array
